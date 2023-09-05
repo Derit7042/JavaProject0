905240077f6f4e58bd0220680424e930430025ac
Junsu An <junsu6752@gmail.com>
2023-09-05 20:10:26 +0900

    2023-09-05-01

--- src/main/java/kr/excel/example/ExcelExample.java
+++ src/main/java/kr/excel/example/ExcelExample.java
@@ -5,6 +5,9 @@ import org.apache.poi.ss.usermodel.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ExcelExample {
     public static void main(String[] args) {
@@ -12,11 +15,45 @@ public class ExcelExample {
             FileInputStream file = new FileInputStream(new File("example.xlsx"));
             Workbook workbook = WorkbookFactory.create(file);
             Sheet sheet = workbook.getSheetAt(0);
-            for(Row row : sheet){
-                for(Cell cell : row){
-                    System.out.print(cell.toString() + "\t");
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case NUMERIC:
+                            if (DateUtil.isCellDateFormatted(cell)) {
+                                Date dateValue = cell.getDateCellValue();
+                                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                String formattedDate = dateFormat.format(dateValue);
+                                System.out.print(formattedDate + "\t");
+                            } else {
+                                double numericValue = cell.getNumericCellValue();
+                                if (numericValue == Math.floor(numericValue)) { int intValue = (int) numericValue;
+                                System.out.print(intValue + "\t");
+                            } else {
+                                System.out.print(numericValue + "\t");
+                        }
+                    }
+                    break;
+                    case STRING:
+                        String stringValue = cell.getStringCellValue();
+                        System.out.print(stringValue + "\t");
+                        break;
+                    case BOOLEAN:
+                        boolean booleanValue = cell.getBooleanCellValue();
+                        System.out.print(booleanValue + "\t");
+                        break;
+                    case FORMULA:
+                        String formulaValue = cell.getCellFormula();
+                        System.out.print(formulaValue + "\t");
+                        break;
+                    case BLANK:
+                        System.out.print("\t");
+                        break;
+                    default:
+                        System.out.print("\t");
+                        break;
+                    }
                 }
-                System.out.println(); // 줄바꿈
+                System.out.println();
             }
             file.close();
             System.out.println("엑셀에서 데이터 읽어오기 성공");
