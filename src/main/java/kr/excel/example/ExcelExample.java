81782562bc5f88549b5c243433d80da4300c1bd1
Junsu An <junsu6752@gmail.com>
2023-09-05 19:58:19 +0900

    2023-09-05-01

---
+++ src/main/java/kr/excel/example/ExcelExample.java
@@ -0,0 +1,27 @@
+package kr.excel.example;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.File;
+
+public class ExcelExample {
+    public static void main(String[] args) {
+        try{
+            FileInputStream file = new FileInputStream(new File("example.xlsx"));
+            Workbook workbook = WorkbookFactory.create(file);
+            Sheet sheet = workbook.getSheetAt(0);
+            for(Row row : sheet){
+                for(Cell cell : row){
+                    System.out.print(cell.toString() + "\t");
+                }
+                System.out.println(); // 줄바꿈
+            }
+            file.close();
+            System.out.println("엑셀에서 데이터 읽어오기 성공");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
